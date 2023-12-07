faa2314693c759a68a0f53f41ff852a40f1677f6
kazenetu <rakudafactory@gmail.com>
2023-12-08 00:20:06 +0900

    クラスエンティティのテストを追加 #4

--- Json2DTO.Test/Domain/Entities/ClassEntityTest.cs
+++ Json2DTO.Test/Domain/Entities/ClassEntityTest.cs
@@ -11,7 +11,7 @@ public class ClassEntityTest
     [Fact]
     public void ExceptionClassNameNull()
     {
-        string className = null;
+        string? className = null;
         var ex = Assert.ThrowsAny<ArgumentException>(() => ClassEntity.Create(className));
         Assert.Equal("className is null", ex.Message);
     }
@@ -25,11 +25,38 @@ public class ClassEntityTest
     }
 
     [Fact]
+    public void ExceptionPropertiesNull()
+    {
+        string className = "ClassName";
+        var classEntity = ClassEntity.Create(className);
+        var ex = Assert.ThrowsAny<ArgumentException>(() => classEntity.AddProperty(null));
+        Assert.Equal("property is null", ex.Message);
+    }
+
+    [Fact]
     public void SuccessPropertiesNone()
     {
         string className = "ClassName";
         var classEntity = ClassEntity.Create(className);
         Assert.Equal("ClassName", classEntity.Name);
         Assert.Equal(0, classEntity.Properties.Count);
+    }
+
+    [Fact]
+    public void SuccessPropertiesExist()
+    {
+        string className = "ClassName";
+        var classEntity = ClassEntity.Create(className);
+        var proprtyType = new PropertyType(typeof(string), false);
+        var propertyValueObject = new PropertyValueObject("Name", proprtyType);
+
+        classEntity.AddProperty(propertyValueObject);
+
+        Assert.Equal("ClassName", classEntity.Name);
+        Assert.Equal(1, classEntity.Properties.Count);
+        Assert.Equal("Name", classEntity.Properties[0].Name);
+        Assert.Equal(string.Empty, classEntity.Properties[0].PropertyTypeClassName);
+        Assert.Equal(PropertyType.Kinds.String, classEntity.Properties[0].Type?.Kind);
+        Assert.Equal(false, classEntity.Properties[0].Type?.IsList);
     }
 }
