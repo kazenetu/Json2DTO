434fdf65bf4cf259861b4d73b44b6d5a159e96b2
kazenetu <rakudafactory@gmail.com>
2023-12-15 23:51:33 +0900

    Infrastructure用インターフェイスを追加 #6

---
+++ Json2DTO/Domain/Interfaces/IFileOutputRepository.cs
@@ -0,0 +1,19 @@
+using Domain.Commands;
+using Domain.Entities;
+using Domain.Results;
+
+namespace Domain.Interfaces;
+
+/// <summary>
+/// ファイル出力リポジトリインターフェース
+/// </summary>
+public interface IFileOutputRepository
+{
+    /// <summary>
+    /// ファイル出力する
+    /// </summary>
+    /// <param name="classInstance">集約エンティティ インスタンス</param>
+    /// <param name="command">コマンドパラメータ</param>
+    /// <returns>出力結果</returns>
+    FileOutputResult OutputResult(ClassesEntity classInstance, FileOutputCommand command);
+}
