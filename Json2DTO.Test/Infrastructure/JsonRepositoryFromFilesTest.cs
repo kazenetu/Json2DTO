de7c900673d76d885a61a07df77da24f8c8c7914
kazenetu <rakudafactory@gmail.com>
2023-12-26 23:06:40 +0900

    [WIP]JSON読み込みリポジトリのテスト:FromFilesを仮追加 #6

---
+++ Json2DTO.Test/Infrastructure/JsonRepositoryFromFilesTest.cs
@@ -0,0 +1,45 @@
+using Domain.Entities;
+using Domain.ValueObjects;
+using Infrastructure;
+using Infrastructure.JsonProperties;
+
+namespace Json2DTO.Test.Infrastructure;
+
+/// <summary>
+/// JSON読み込みリポジトリのテスト:FromFiles
+/// </summary>
+public class JsonRepositoryFromFilesTest: IDisposable
+{
+    /// <summary>
+    /// 生成したファイルを格納したディレクトリパス
+    /// </summary>
+    private string CreatedDirectoryPath = "fromFiles";
+
+    /// <summary>
+    /// Teardown
+    /// </summary>
+    public void Dispose()
+    {
+        // ディレクトリ削除
+        Directory.Delete(CreatedDirectoryPath, true);
+    }
+
+    /// <summary>
+    /// JSONファイル作成
+    /// </summary>
+    /// <param name="json">JSON文字列</param>
+    /// <param name="fileName">ファイル名</param>
+    private void CreateFile(string json, string fileName)
+    {
+        // フォルダの確認と生成
+        if(!Directory.Exists(CreatedDirectoryPath))
+        {
+            Directory.CreateDirectory(CreatedDirectoryPath);
+        }
+        var filePath = Path.Combine(CreatedDirectoryPath, fileName);
+
+        // ファイル出力
+        File.WriteAllText(filePath, json);
+    }
+
+}
