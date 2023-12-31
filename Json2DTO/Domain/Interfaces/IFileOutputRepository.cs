e36364ef8b2d65ac288830fada4660102811df8c
kazenetu <rakudafactory@gmail.com>
2023-12-31 13:25:29 +0900

    複数集約エンティティのファイル出力メソッドを追加 #6

--- Json2DTO/Domain/Interfaces/IFileOutputRepository.cs
+++ Json2DTO/Domain/Interfaces/IFileOutputRepository.cs
@@ -10,6 +10,14 @@ namespace Domain.Interfaces;
 public interface IFileOutputRepository
 {
     /// <summary>
+    /// 複数ファイルを出力する
+    /// </summary>
+    /// <param name="classInstances">複数集約エンティティ</param>
+    /// <param name="command">コマンドパラメータ</param>
+    /// <returns>出力結果</returns>
+    IReadOnlyList<FileOutputResult> OutputResults(IReadOnlyList<ClassesEntity> classInstances, FileOutputCommand command);
+
+    /// <summary>
     /// ファイル出力する
     /// </summary>
     /// <param name="classInstance">集約エンティティ インスタンス</param>
