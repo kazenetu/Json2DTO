15a4c3ba6fdc69a829a114c05d78a9a3b0b8d239
kazenetu <rakudafactory@gmail.com>
2024-01-03 20:45:34 +0900

    CS用コマンドクラスを追加  #8

---
+++ Json2DTO/Application/Commands/CSharpCommand.cs
@@ -0,0 +1,12 @@
+namespace Appplication.Commands;
+
+/// <summary>
+/// C#用コマンドクラス
+/// </summary>
+/// <param name="NameSpace">名前空間</param>
+/// <param name="RootPath">出力ルートパス</param>
+/// <param name="RootClassName">ルートクラス名(ファイル名)</param>
+/// <param name="IndentSpaceCount">インデントスペース数</param>
+/// <param name="Prefix">固定プレフィックス</param>
+/// <param name="Suffix">固定サフィックス</param>
+public record CSharpCommand(string NameSpace, string RootPath, string RootClassName, int IndentSpaceCount = 2, string Prefix = "", string Suffix = "") : CommonCommand(Prefix, Suffix);
