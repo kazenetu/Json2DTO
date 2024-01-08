662d00bc3676762b78ba04eb78d8d5c74f750b12
kazenetu <rakudafactory@gmail.com>
2024-01-08 17:52:28 +0900

    ClassesApplicationテスト用拡張メソッド追加 #8

---
+++ Json2DTO.Test/Application/Extensions/ClassesApplicationExtensions.cs
@@ -0,0 +1,56 @@
+using Appplication;
+using Domain.Interfaces;
+
+namespace Json2DTO.Test.Application.Extensions;
+
+/// <summary>
+/// ClassesApplicationテスト用拡張メソッド
+/// </summary>
+public static class ClassesApplicationExtensions
+{
+    /// <summary>
+    /// JsonRepositoryをnullにする
+    /// </summary>
+    /// <param name="application">ClassesApplicationインスタンス</param>
+    public static void ClearJsonRepository(this ClassesApplication application) 
+    {
+        var targetName = "JsonRepository";
+
+        // フィールドを検索
+        var filelds = application.GetType().GetFields(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        for (int i = 0; i < filelds.Length; i++)
+        {
+            // インターフェイス以外は処理対象外
+            if (!filelds[i].FieldType.IsInterface) continue;
+
+            // targetName以外は処理対象外
+            if(filelds[i].Name != targetName) continue;
+
+            // インターフェイスフィールドをnull
+            filelds[i].SetValue(application, null);
+        }
+    }
+
+    /// <summary>
+    /// FileOutputRepositoryをnullにする
+    /// </summary>
+    /// <param name="application">ClassesApplicationインスタンス</param>
+    public static void ClearFileOutputRepository(this ClassesApplication application) 
+    {
+        var targetName = "FileOutputRepository";
+
+        // フィールドを検索
+        var filelds = application.GetType().GetFields(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        for (int i = 0; i < filelds.Length; i++)
+        {
+            // インターフェイス以外は処理対象外
+            if (!filelds[i].FieldType.IsInterface) continue;
+
+            // targetName以外は処理対象外
+            if(filelds[i].Name != targetName) continue;
+
+            // インターフェイスフィールドをnull
+            filelds[i].SetValue(application, null);
+        }
+    }
+}
