81e522d5484640b467958c0958ed0d2fa978c276
kazenetu <rakudafactory@gmail.com>
2024-01-17 23:42:36 +0900

    エントリメソッド追加 #12

--- Json2DTO/Presentation/Console/Program.cs
+++ Json2DTO/Presentation/Console/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Appplication;
+using Domain.Interfaces;
+using Infrastructure;
+using TinyDIContainer;
+
+namespace Presentation.Console;
+
+internal class Program
+{
+    /// <summary>
+    /// エントリメソッド
+    /// </summary>    
+    private static void Main(string[] args)
+    {
+        // 入力チェック
+        if(args.Length > 2)
+        {
+            System.Console.WriteLine();
+            System.Console.Write("Input parameters!");
+            System.Console.Write("\"");
+            System.Console.Write("OutputPath ");
+            System.Console.Write("\"DirectoryPath/FilePath/JsonString\"");
+            System.Console.Write("[NameSpace]");
+            System.Console.Write("\"");
+            System.Console.WriteLine();
+            return;
+        }
+
+        // DI設定
+        DIContainer.Add<IFileOutputRepository, FileOutputRepository>();
+        DIContainer.Add<IJsonRepository, JsonRepository>();
+    }
+}
